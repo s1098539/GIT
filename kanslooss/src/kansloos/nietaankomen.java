a2cb8cfeadacbf50cb143a3a86289997a7199e1c
Lion <s1098539@student.hsleiden.nl>
2017-05-23 22:45:47 +0200

    a

---
+++ kanslooss/src/kansloos/nietaankomen.java
@@ -0,0 +1,6 @@
+package kansloos;
+
+public class nietaankomen {
+	
+}
+public class void 
---
+++ kanslooss/src/kansloos/nogeens.java
@@ -0,0 +1,10 @@
+package kansloos;
+
+public class nogeens {
+
+	public static void main(String[] args) {
+		System.out.println("Dit moet geprint worden");
+
+	}
+
+}
