d8bf051739e4cb27ad6bd6ee4702f2be8f94a662
Joepin8 <joep_oonk@hotmail.com>
2017-05-23 16:27:26 +0200

    Dit kan gewoon

---
+++ Probably not usable/src/Derp.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Joep Oonk on 23-5-2017.
+ */
+public class Derp {
+}
--- Workspace/flashpoint/src/flashpoint/RMI.java
+++ Workspace/flashpoint/src/flashpoint/RMI.java
@@ -1,4 +1,8 @@
 package flashpoint;
+
+import java.util.Scanner;
+
 //testing more git
 public class RMI {
 }
+
