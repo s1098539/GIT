3635de39fd1f7602d927086e5a1e3c10a427a77a
Lion <s1098539@student.hsleiden.nl>
2017-05-24 15:03:11 +0200

    Bewegen

--- Workspace/flashpoint/src/flashpoint/Main.java
+++ Workspace/flashpoint/src/flashpoint/Main.java
@@ -5,9 +5,14 @@ public class Main {
 
     public static void main(String[] args) {
         Speelveld speelveld = new Speelveld();
+        System.out.println("1");
         Bewegen bewegen = new Bewegen();
+        System.out.println("2");
         Speler speler = new Speler("Joep", Kleur.ROOD);
+        System.out.println("3");
+        speelveld.getVakken()[2][2].addSpeler(speler);
         bewegen.run(0,speler,speelveld.getVakken());
+        System.out.println("4");
 
     }
 }
