e1e24a476de34e75c799954eebfbd468dbba8464
Lion <s1098539@student.hsleiden.nl>
2017-05-25 15:30:46 +0200

    testview

---
+++ TestView/src/sample/Main.java
@@ -0,0 +1,30 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        System.out.println("Application has been launched.");
+
+
+
+
+
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 1000, 700));
+        primaryStage.show();
+
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
