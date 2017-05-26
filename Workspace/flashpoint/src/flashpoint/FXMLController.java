d93120972d7b2533c5e74e84f7f9eb0fb8b071c0
Lion <s1098539@student.hsleiden.nl>
2017-05-26 16:52:51 +0200

    Dobbelsteen klasse gemaakt en getest

--- Workspace/flashpoint/src/flashpoint/FXMLController.java
+++ Workspace/flashpoint/src/flashpoint/FXMLController.java
@@ -16,6 +16,12 @@ public class FXMLController {
     @FXML private void initialize() {
         ImageView iv2 = new ImageView(new Image("http://www.i2symbol.com/images/cool-letters/katakana/katakana_middle_dot_u30FB_icon_256x256.png"));
 
+        Dobbelsteen dobbelsteen = new Dobbelsteen(8);
+        dobbelsteen.gooi();
+        System.out.println(dobbelsteen.getWaarde());
+        dobbelsteen.flip();
+        System.out.println(dobbelsteen.getWaarde());
+
         Speelveld speelveld = new Speelveld();
         Bewegen bewegen = new Bewegen();
         Speler speler1 = new Speler("Joep", Kleur.ROOD, iv2);
