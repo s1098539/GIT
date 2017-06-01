4e77d2ba724fdd706dddcebee208ce42be15bd0d
Lion <s1098539@student.hsleiden.nl>
2017-06-01 11:56:45 +0200

    Had een speler nodig dus die hebben we nu \o/

--- Flashspace/src/sample/Speler.java
+++ Flashspace/src/sample/Speler.java
@@ -1,7 +1,13 @@
+package sample;
 public class Speler {
 
 	public int naam;
 	public int character;
 	public int kleur;
 
+    public Speler(int naam, int character, int kleur) {
+        this.naam = naam;
+        this.character = character;
+        this.kleur = kleur;
+    }
 }
