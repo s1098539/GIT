ca4f591c55c19414051696b8bfaaa822c1f4a398
Lion <s1098539@student.hsleiden.nl>
2017-06-01 13:07:38 +0200

    Speler actie, beweeg actie is klaar

--- Flashspace/src/sample/Speler.java
+++ Flashspace/src/sample/Speler.java
@@ -1,13 +1,57 @@
 package sample;
 public class Speler {
 
-	public int naam;
-	public int character;
-	public int kleur;
+	public String naam;
+	public Character character;
+	public Kleur kleur;
+	public int x;
+	public int y;
 
-    public Speler(int naam, int character, int kleur) {
+    public Speler(String naam, Character character, Kleur kleur, int x, int y) {
         this.naam = naam;
         this.character = character;
         this.kleur = kleur;
+        this.x = x;
+        this.y = y;
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public void setNaam(String naam) {
+        this.naam = naam;
+    }
+
+    public Character getCharacter() {
+        return character;
+    }
+
+    public void setCharacter(Character character) {
+        this.character = character;
+    }
+
+    public Kleur getKleur() {
+        return kleur;
+    }
+
+    public void setKleur(Kleur kleur) {
+        this.kleur = kleur;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
     }
 }
