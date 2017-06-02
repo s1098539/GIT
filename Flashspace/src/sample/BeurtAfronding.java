e5eaeb566b00e20db108a05d2118aeac2094dc4b
Joepin8 <joep_oonk@hotmail.com>
2017-06-02 13:00:41 +0200

    handle explosie AF

--- Flashspace/src/sample/BeurtAfronding.java
+++ Flashspace/src/sample/BeurtAfronding.java
@@ -20,18 +20,20 @@ public class BeurtAfronding {
         this.veld = veld;
     }
 
+
+
     public void newRook() {
         d6.gooi();
         d8.gooi();
         int x = d8.getWaarde();
         int y = d6.getWaarde();
-        if (veld.getVakken()[x][y].getObjecten()[6] == null) {
+        if (veld.getVak(x,y).getObjecten()[6] == null) {
             Object.Rook rook = new Object.Rook();
-            veld.getVakken()[x][y].addObject(rook);
-        } else if (veld.getVakken()[x][y].getObjecten()[6].getNaam().equals("Vuur")) {
+            veld.getVak(x,y).addObject(rook);
+        } else if (veld.getVak(x,y).getObjecten()[6].getNaam().equals("Vuur")) {
             Object.Vuur vuur = new Object.Vuur();
-            veld.getVakken()[x][y].addObject(vuur);
-        } else if (veld.getVakken()[x][y].getObjecten()[6].getNaam().equals("Vuur")) {
+            veld.getVak(x,y).addObject(vuur);
+        } else if (veld.getVak(x,y).getObjecten()[6].getNaam().equals("Vuur")) {
             handleExplosie(x, y);
         }
     }
@@ -42,71 +44,70 @@ public class BeurtAfronding {
         int obstakel;
         boolean doorgaan;
         int teller;
-        for(int richting = 0; richting<4; richting++) {
-            doorgaan = true;
+        Vak vak;
+        for (int richting = 0; richting < 4; richting++) {
             teller = 0;
-            if (richting == 0){
-                obstakel = veld.getVakken()[x][y].checkObstakels(richting);
-                if (obstakel < 2 || obstakel == 3) {
-                    schade(richting, x, y);
-                    //doorgaan = false;
-                }
-                else{
-                    do {
-
-                        obstakel = veld.getVakken()[x][y-teller].checkObstakels(richting);
-
-                        if (y - teller >0 && (obstakel == 2 || obstakel == 4 || obstakel == 5)) {
-                            veld.getVakken()[x][y - teller].addObject(new Object.Vuur());
-
-                        }
-                        teller++;
-
-                    } while (veld.getVakken()[x][y-teller].getObjecten()[6].getNaam().equals("Vuur"));
-
-            }
-
-
-            }
-            if (richting == 1){
-                while ((x + teller < 9) && doorgaan) {
-                    obstakel = veld.getVakken()[x + teller][y].checkObstakels(richting);
-                    if (obstakel < 2 || obstakel == 3) {
-                        schade(richting, x+teller, y);
-                        doorgaan = false;
-                    } else if (x+teller<9) {
-                        teller++;
-                        veld.getVakken()[x+teller][y].addObject(new Object.Vuur());
-                    }
-                }
-            }
-            if (richting == 2){
-                while ((y + teller < 7) && doorgaan) {
-                    obstakel = veld.getVakken()[x][y + teller].checkObstakels(richting);
-                    if (obstakel < 2 || obstakel == 3) {
-                        schade(richting, x, y+teller);
-                        doorgaan = false;
-                    } else if (y+teller>0) {
-                        teller++;
-                        veld.getVakken()[x][y + teller].addObject(new Object.Vuur());
-                    }
-                }
-            }
-            if (richting == 3){
-                while ((x - teller > 0) && doorgaan) {
-                    obstakel = veld.getVakken()[x + teller][y].checkObstakels(richting);
-                    if (obstakel < 2 || obstakel == 3) {
-                        schade(richting, x - teller, y);
+            doorgaan = true;
+            while (y - teller > 0 && doorgaan) {
+                vak = veld.getVak(x, y - teller);
+                if (vak.checkObstakels(richting) == 2 || vak.checkObstakels(richting) > 3) {
+                    teller++;
+                    if (!vak.getObjecten()[6].getNaam().equals("Vuur")) {
                         doorgaan = false;
                     }
-                    else if (x-teller>0) {
-                        teller++;
-                        veld.getVakken()[x-teller][y].addObject(new Object.Vuur());
+                    vak.addObject(new Object.Vuur());
+                }
+                else {
+                    schade(richting, x, (y - teller));
+                    doorgaan = false;
+                }
+            }
+            while (x + teller < 9 && doorgaan) {
+                vak = veld.getVak(x+teller, y);
+                if (vak.checkObstakels(richting) == 2 || vak.checkObstakels(richting) > 3) {
+                    teller++;
+                    if (!vak.getObjecten()[6].getNaam().equals("Vuur")) {
+                        doorgaan = false;
                     }
+                    vak.addObject(new Object.Vuur());
+                }
+                else {
+                    schade(richting, x+teller, y);
+                    doorgaan = false;
+                }
+            }
+            while (y + teller < 7 && doorgaan) {
+                vak = veld.getVak(x, y + teller);
+                if (vak.checkObstakels(richting) == 2 || vak.checkObstakels(richting) > 3) {
+                    teller++;
+                    if (!vak.getObjecten()[6].getNaam().equals("Vuur")) {
+                        doorgaan = false;
+                    }
+                    vak.addObject(new Object.Vuur());
+                }
+                else {
+                    schade(richting, x, (y + teller));
+                    doorgaan = false;
+                }
+            }
+            while (x - teller > 0 && doorgaan) {
+                vak = veld.getVak(x - teller, y);
+                if (vak.checkObstakels(richting) == 2 || vak.checkObstakels(richting) > 3) {
+                    teller++;
+                    if (!vak.getObjecten()[6].getNaam().equals("Vuur")) {
+                        doorgaan = false;
+                    }
+                    vak.addObject(new Object.Vuur());
+                }
+                else {
+                    schade(richting, x - teller, y);
+                    doorgaan = false;
                 }
             }
         }
     }
+
+
 
 //    public void bomberMan(int x, int y) {//Work in progress - Joep
 //        int obstakel;
@@ -167,42 +168,42 @@ public class BeurtAfronding {
 //        }
 //    }
 //
-
     public void schade(int richting, int x, int y){
-        int obstakel = veld.getVakken()[x][y].checkObstakels(richting);
+        int obstakel = veld.getVak(x,y).checkObstakels(richting);
         switch(obstakel) {
             case 0:
-                veld.getVakken()[x][y].setObstakel(richting, 1);
+                veld.getVak(x,y).setObstakel(richting, 1);
                 schadeLoop(richting,1,x,y);
                 break;
             case 1:
-                veld.getVakken()[x][y].setObstakel(richting, 1);
+                veld.getVak(x,y).setObstakel(richting, 1);
                 schadeLoop(richting,2,x,y);
                 break;
             case 3:
-                veld.getVakken()[x][y].setObstakel(richting, 5);
+                veld.getVak(x,y).setObstakel(richting, 5);
                 schadeLoop(richting,5,x,y);
                 break;
         }
 
-    }
+}
 
     public void schadeLoop(int richting, int obstakel, int x, int y) {
         switch(richting) {
             case 0:
-                veld.getVakken()[x][y - 1].setObstakel(2, obstakel);
+                veld.getVak(x,y-1).setObstakel(2, obstakel);
                 break;
             case 1:
-                veld.getVakken()[x + 1][y].setObstakel(3, obstakel);
+                veld.getVak(x+1,y).setObstakel(3, obstakel);
                 break;
             case 2:
-                veld.getVakken()[x][y + 1].setObstakel(4, obstakel);
+                veld.getVak(x,y+1).setObstakel(4, obstakel);
                 break;
             case 3:
-                veld.getVakken()[x - 1][y].setObstakel(1, obstakel);
+                veld.getVak(x-1,y).setObstakel(1, obstakel);
                 break;
         }
-        }
+    }
+
 
 	public void handleVonkoverslag() {
 
