40133351c8e866c2215b79c84a2f0aa3d37b01a5
Lion <s1098539@student.hsleiden.nl>
2017-06-03 17:23:25 +0200

    Bordspel als achtergrond toegevoegd

--- Flashspace/src/sample/Controller.java
+++ Flashspace/src/sample/Controller.java
@@ -31,7 +31,7 @@ public class Controller {
 
         for(int x = 0; x <10; x++) {
             for (int y = 0; y < 8; y++) {
-                veld.getVak(x, y).addObject(new Object.Vuur());
+                veld.getVak(x, y).addObject(new Object.Rook());
             }
         }
 
@@ -46,12 +46,16 @@ public class Controller {
                 veld.getVak(x, y).addObject(new Object.PersoonVanAandacht());
             }
         }
-        for(int x = 0; x <10; x++) {
-            for (int y = 0; y < 8; y++) {
-                for (int z = 0; z < 4; z++) {
-                    veld.getVak(x, y).setObstakel(z, 0);
-                }
-            }
+//        for(int x = 0; x <10; x++) {
+//            for (int y = 0; y < 8; y++) {
+//                for (int z = 0; z < 4; z++) {
+//                    veld.getVak(x, y).setObstakel(z, 0);
+//                }
+//            }
+//        }
+
+        for (int i = 0; i < 20; i++) {
+            ba.newRook();
         }
         makeWalls();
         update();
