3aa82185af50920fa9d2f8f9651f90cd34c2c2b9
Lion <s1098539@student.hsleiden.nl>
2017-06-04 01:58:16 +0200

    Oke, nu werkt Vonko ECHT

--- Flashspace/src/sample/BeurtAfronding.java
+++ Flashspace/src/sample/BeurtAfronding.java
@@ -43,35 +43,36 @@ public class BeurtAfronding {
             for (int x = 0; x < 10; x++) {
                 for (int y = 0; y < 8; y++) {
                     vak = veld.getVak(x, y);
-                        if (vak.getObjecten()[6] != null && vak.getObjecten()[6].getNaam().equals("Vuur")) {
-                            for (int i = 0; i < 4; i++)
-                                if (vak.checkObstakels(i) == 2 || vak.checkObstakels(i) > 3) {
-                                    switch (i) {
-                                        case 0:
-                                            if (y > 0 && veld.getVak(x, y - 1).getObjecten()[6] != null && veld.getVak(x, y - 1).getObjecten()[6].getNaam().equals("Rook")) {
-                                                veld.getVak(x, y - 1).addObject(new Object.Vuur());
-                                                loop = true;
-                                            }
-                                            break;
-                                        case 1:
-                                            if (x < 9 && veld.getVak(x+1,y).getObjecten()[6] != null && veld.getVak(x + 1, y).getObjecten()[6].getNaam().equals("Rook")) {
-                                                    veld.getVak(x + 1, y).addObject(new Object.Vuur());
-                                            }
-                                            break;
-                                        case 2:
-                                            if (y < 7 && veld.getVak(x, y + 1).getObjecten()[6] != null && veld.getVak(x, y + 1).getObjecten()[6].getNaam().equals("Rook")) {
-                                                veld.getVak(x, y + 1).addObject(new Object.Vuur());
-                                            }
-                                            break;
-                                        case 3:
-                                            if (x > 0 && veld.getVak(x - 1, y).getObjecten()[6] != null && veld.getVak(x - 1, y).getObjecten()[6].getNaam().equals("Rook")) {
-                                                    veld.getVak(x - 1, y).addObject(new Object.Vuur());
-                                                loop = true;
-                                            }
-                                            break;
-                                    }
+                    if (vak.getObjecten()[6] != null && vak.getObjecten()[6].getNaam().equals("Vuur")) {
+                        for (int i = 0; i < 4; i++) {
+                            if (vak.checkObstakels(i) == 2 || vak.checkObstakels(i) > 3) {
+                                switch (i) {
+                                    case 0:
+                                        if (y > 0 && veld.getVak(x, y - 1).getObjecten()[6] != null && veld.getVak(x, y - 1).getObjecten()[6].getNaam().equals("Rook")) {
+                                            veld.getVak(x, y - 1).addObject(new Object.Vuur());
+                                            loop = true;
+                                        }
+                                        break;
+                                    case 1:
+                                        if (x < 9 && veld.getVak(x + 1, y).getObjecten()[6] != null && veld.getVak(x + 1, y).getObjecten()[6].getNaam().equals("Rook")) {
+                                            veld.getVak(x + 1, y).addObject(new Object.Vuur());
+                                        }
+                                        break;
+                                    case 2:
+                                        if (y < 7 && veld.getVak(x, y + 1).getObjecten()[6] != null && veld.getVak(x, y + 1).getObjecten()[6].getNaam().equals("Rook")) {
+                                            veld.getVak(x, y + 1).addObject(new Object.Vuur());
+                                        }
+                                        break;
+                                    case 3:
+                                        if (x > 0 && veld.getVak(x - 1, y).getObjecten()[6] != null && veld.getVak(x - 1, y).getObjecten()[6].getNaam().equals("Rook")) {
+                                            veld.getVak(x - 1, y).addObject(new Object.Vuur());
+                                            loop = true;
+                                        }
+                                        break;
                                 }
+                            }
                         }
+                    }
                 }
             }
         }
--- Flashspace/src/sample/Controller.java
+++ Flashspace/src/sample/Controller.java
@@ -32,11 +32,11 @@ public class Controller {
 //            veld.getVakken()[c][2].setObstakel(0,1);
 //        }
 //
-        for(int x = 0; x <10; x++) {
-            for (int y = 0; y < 8; y++) {
-                veld.getVak(x, y).addObject(new Object.Vuur());
-            }
-        }
+//        for(int x = 0; x <10; x++) {
+//            for (int y = 0; y < 8; y++) {
+//                veld.getVak(x, y).addObject(new Object.Vuur());
+//            }
+//        }
 //
 //        for(int x = 0; x <10; x++) {
 //            for (int y = 0; y < 8; y++) {
@@ -63,37 +63,37 @@ public class Controller {
 
 
 
-//        //Vonko naar boven test (linker verticale lijn)
-//        veld.getVak(3, 0).addObject(new Object.Rook());
-//        veld.getVak(3, 1).addObject(new Object.Rook());
-//        veld.getVak(3, 2).addObject(new Object.Rook());
-//        veld.getVak(3,3).addObject(new Object.Rook());
-//        veld.getVak(3, 4).addObject(new Object.Rook());
-//        veld.getVak(3, 5).addObject(new Object.Vuur());
-//
-//        //Vonko naar beneden test (rechter verticale lijn)
-//        veld.getVak(5, 0).addObject(new Object.Vuur());
-//        veld.getVak(5, 1).addObject(new Object.Rook());
-//        veld.getVak(5, 2).addObject(new Object.Rook());
-//        veld.getVak(5,3).addObject(new Object.Rook());
-//        veld.getVak(5, 4).addObject(new Object.Rook());
-//        veld.getVak(5, 5).addObject(new Object.Rook());
-//
-//        //Vonko naar links test (linker horizontale lijn)
-//        veld.getVak(0, 7).addObject(new Object.Rook());
-//        veld.getVak(1, 7).addObject(new Object.Rook());
-//        veld.getVak(2, 7).addObject(new Object.Rook());
-//        veld.getVak(3, 7).addObject(new Object.Rook());
-//        veld.getVak(4, 7).addObject(new Object.Vuur());
-//
-//        //Vonka naar rechts test (rechter horizontale lijn)
-//        veld.getVak(6, 7).addObject(new Object.Vuur());
-//        veld.getVak(7, 7).addObject(new Object.Rook());
-//        veld.getVak(8, 7).addObject(new Object.Rook());
-//        veld.getVak(9, 7).addObject(new Object.Rook());
+        //Vonko naar boven test (linker verticale lijn)
+        veld.getVak(3, 0).addObject(new Object.Rook());
+        veld.getVak(3, 1).addObject(new Object.Rook());
+        veld.getVak(3, 2).addObject(new Object.Rook());
+        veld.getVak(3,3).addObject(new Object.Rook());
+        veld.getVak(3, 4).addObject(new Object.Rook());
+        veld.getVak(3, 5).addObject(new Object.Vuur());
+
+        //Vonko naar beneden test (rechter verticale lijn)
+        veld.getVak(5, 0).addObject(new Object.Vuur());
+        veld.getVak(5, 1).addObject(new Object.Rook());
+        veld.getVak(5, 2).addObject(new Object.Rook());
+        veld.getVak(5,3).addObject(new Object.Rook());
+        veld.getVak(5, 4).addObject(new Object.Rook());
+        veld.getVak(5, 5).addObject(new Object.Rook());
+
+        //Vonko naar links test (linker horizontale lijn)
+        veld.getVak(0, 7).addObject(new Object.Rook());
+        veld.getVak(1, 7).addObject(new Object.Rook());
+        veld.getVak(2, 7).addObject(new Object.Rook());
+        veld.getVak(3, 7).addObject(new Object.Rook());
+        veld.getVak(4, 7).addObject(new Object.Vuur());
+
+        //Vonka naar rechts test (rechter horizontale lijn)
+        veld.getVak(6, 7).addObject(new Object.Vuur());
+        veld.getVak(7, 7).addObject(new Object.Rook());
+        veld.getVak(8, 7).addObject(new Object.Rook());
+        veld.getVak(9, 7).addObject(new Object.Rook());
 
 
-        ba.newRook();
+        ba.handleVonkoverslag();
 
         view.update();
 
