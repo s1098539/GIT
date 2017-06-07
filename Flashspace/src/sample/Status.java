eae00664520ac0d2643486b04537ecf188c74827
Lion <s1098539@student.hsleiden.nl>
2017-06-07 12:43:12 +0200

    Status enum

--- Flashspace/src/sample/Status.java
+++ Flashspace/src/sample/Status.java
@@ -4,14 +4,16 @@ package sample;
  * Created by lion on 06-06-17.
  */
 public enum Status {
-    MUUR(false), MUUR1(false), MUUR2(true), DEURO(true), DEURD(false), LEEG(true);
+    MUUR(false, 0), MUUR1(false, 1), MUUR2(true, 2), DEURO(true, 3), DEURD(false, 4), LEEG(true, 5);
     private final boolean begaanbaar;
+    private final int index;
 
     public boolean isBegaanbaar() {
         return begaanbaar;
     }
 
-    Status(boolean begaanbaar) {
+    Status(boolean begaanbaar, int index) {
         this.begaanbaar = begaanbaar;
+        this.index = index;
     }
 }
