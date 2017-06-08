e4aad030c8bc16c68db1f55a6659fcd7b69423e9
Lion <s1098539@student.hsleiden.nl>
2017-06-08 17:24:55 +0200

    MVC garbage, werkt niet. Nee echt niet, get out of here.

--- Flashspace/src/sample/Controller.java
+++ Flashspace/src/sample/Controller.java
@@ -1,270 +1,48 @@
 package sample;
 
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class Controller {
-    @FXML
-    private GridPane gridpane;
-    @FXML
-    private Button btnUP;
-    @FXML
-    private Button btnLEFT;
-    @FXML
-    private Button btnRIGHT;
-    @FXML
-    private Button btnDOWN;
-    @FXML
-    private ImageView imgHakken;
-    @FXML
-    private ImageView imgOpenendeur;
-    @FXML
-    private ImageView imgBrandblusser;
-    @FXML
-    private Button btnSpecial;
 
-    public GridPane getGridpane() {
-        return gridpane;
+
+    private View view;
+    private BeurtAfronding beurtAfronding;
+    private Speelveld speelveld;
+    private Speler speler;
+
+    public Speelveld getSpeelveld() {
+        return speelveld;
     }
 
-    public void setGridpane(GridPane gridpane) {
-        this.gridpane = gridpane;
+    public Speler getSpeler() {
+        return speler;
     }
 
-    Boolean hakken = false;
-    Boolean deurinteractie = false;
-    Boolean brandblusser = false;
+    public Controller(View view, BeurtAfronding beurtAfronding, Speelveld speelveld, Speler speler) {
+        this.view = view;
+        this.beurtAfronding = beurtAfronding;
+        this.speelveld = speelveld;
+        this.speler = speler;
+        this.view.addCalculationListener(listenForCalcButton);
+    }
 
-    //
-//    private FlowPane[][]fp = new FlowPane[10][8];
-//    private Speelveld veld = new Speelveld();
-//    ImageView[]ivarray = new ImageView[720];
-//    private View view = new View(fp,veld);
-//    private BeurtAfronding ba = new BeurtAfronding(veld);
-////    private Object.Speler speler = new Object.Speler("Lion", Kleur.ZWART, 4,3);
-////    SpelerActie sa = new SpelerActie(speler, veld);
-//    private Boolean hakken = false;
-//    private Boolean openendeur;
-//
-    @FXML
-    public void initialize() {
-        Speelveld veld = new Speelveld();
-/*        for(int x = 0; x<10; x++) {
-            for(int y = 0; y<8; y++) {
-                veld.getVak(x,y).vuurPlaats(Fiche.ROOK);
-                veld.getVak(x,y).setHotspot(true);
-                veld.getVak(x,y).setPersoon(true);
-                veld.getVak(x,y).setStoffen(true);*/
-//                veld.getVak(x,y).setBoven(Status.LEEG);
-//                veld.getVak(x,y).setOnder(Status.LEEG);
-//                veld.getVak(x,y).setLinks(Status.LEEG);
-//                veld.getVak(x,y).setRechts(Status.LEEG);
-            //}
-       // }
+    public class CalculateListener implements ActionListener{
 
-        View view = new View();
-        Speler speler = new Speler("Joep", Kleur.GROEN, 2,7, veld);
-        BeurtAfronding ba = new BeurtAfronding(veld);
-        view.imageViewsFactory();
-        System.out.println("hoi");
-        view.flowPanesFactory();
-        int teller = 0;
-        for(int y = 0; y<8; y++) {
-            for(int x = 0; x<10; x++) {
-                gridpane.add(view.getFlowPanes()[teller],x,y);
-                teller++;
-            }
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
         }
-        veld.getVak(5,5).setHotspot(true);
-        veld.setMuren();
-        veld.getVak(3,3).vuurPlaats(Fiche.VUUR);
-        view.imageSetter(veld, speler);
-
-
-
-////        veld.getVak(speler.getX(),speler.getY()).addObject(speler);
-//
-//
-//        System.out.println("test");
-//        for(int x = 0; x <10; x++) {
-//            for (int y = 0; y < 8; y++) {
-//                fp[x][y] = new FlowPane();
-//                gridpane.add(fp[x][y],x,y);
-//            }
-//        }
-//        veld.setMuren();
-//
-//
-//
-////        for(int c=1; c<9; c++){
-////            veld.getVakken()[c][2].setObstakel(0,1);
-////        }
-////
-////        for(int x = 0; x <10; x++) {
-////            for (int y = 0; y < 8; y++) {
-////                veld.getVak(x, y).addObject(new Object.Vuur());
-////            }
-////        }
-////
-////        for(int x = 0; x <10; x++) {
-////            for (int y = 0; y < 8; y++) {
-////                veld.getVak(x, y).addObject(new Object.BrandHaard());
-////            }
-////        }
-////
-////        for(int x = 0; x <10; x++) {
-////            for (int y = 0; y < 8; y++) {
-////                veld.getVak(x, y).addObject(new Object.PersoonVanAandacht());
-////            }
-////        }
-////
-//
-////        veld.getVak(2,2).addObject(new Object.GevaarlijkeStof());
-//
-//
-//
-//
-//
-//
-//        //Vonko naar boven test (linker verticale lijn)
-////        veld.getVak(9, 7).addObject(new Object.GevaarlijkeStof());
-////        veld.getVak(3, 0).addObject(new Object.Rook());
-////        veld.getVak(3, 1).addObject(new Object.Rook());
-////        veld.getVak(3, 2).addObject(new Object.Rook());
-////        veld.getVak(3,3).addObject(new Object.Rook());
-////        veld.getVak(3, 4).addObject(new Object.Rook());
-////        veld.getVak(3, 5).addObject(new Object.Vuur());
-////
-////        //Vonko naar beneden test (rechter verticale lijn)
-////        veld.getVak(5, 0).addObject(new Object.Vuur());
-////        veld.getVak(5, 1).addObject(new Object.Rook());
-////        veld.getVak(5, 2).addObject(new Object.Rook());
-////        veld.getVak(5,3).addObject(new Object.Rook());
-////        veld.getVak(5, 4).addObject(new Object.Rook());
-////        veld.getVak(5, 5).addObject(new Object.Rook());
-////
-////        //Vonko naar links test (linker horizontale lijn)
-////        veld.getVak(0, 7).addObject(new Object.Rook());
-////        veld.getVak(1, 7).addObject(new Object.Rook());
-////        veld.getVak(2, 7).addObject(new Object.Rook());
-////        veld.getVak(3, 7).addObject(new Object.Rook());
-////        veld.getVak(4, 7).addObject(new Object.Vuur());
-////
-////        //Vonka naar rechts test (rechter horizontale lijn)
-////        veld.getVak(6, 7).addObject(new Object.Vuur());
-////        veld.getVak(7, 7).addObject(new Object.Rook());
-////        veld.getVak(8, 7).addObject(new Object.Rook());
-////        veld.getVak(9, 7).addObject(new Object.Rook());
-//
-//
-//
-//        // ba.handleVonkoverslag();
-//        // ba.newRook();
-//        view.update();
-//        Vak vak = new Vak();
-//        vak.setBoven(Vak.Status.MUUR1);
-//        System.out.println(vak.getBoven());
-//
-        btnUP.setOnAction(event -> {
-            if(hakken) {
-                speler.muurInteractie(Richting.BOVEN);
-            }else if(deurinteractie){
-                speler.deurInteractie(Richting.BOVEN);
-            }else if(brandblusser){
-                speler.blusActie(Richting.BOVEN);
-            } else {
-                speler.beweegwActie(Richting.BOVEN);
-            }
-            view.imageSetter(veld,speler);
-        });
-
-        btnLEFT.setOnAction(event -> {
-            if(hakken) {
-                speler.muurInteractie(Richting.LINKS);
-            }else if(deurinteractie){
-                speler.deurInteractie(Richting.LINKS);
-            }else if(brandblusser){
-                speler.blusActie(Richting.LINKS);
-            } else {
-                speler.beweegwActie(Richting.LINKS);
-            }
-            view.imageSetter(veld,speler);
-        });
-
-        btnRIGHT.setOnAction(event -> {
-            if(hakken) {
-                speler.muurInteractie(Richting.RECHTS);
-            }else if(deurinteractie){
-                speler.deurInteractie(Richting.RECHTS);
-            }else if(brandblusser){
-                speler.blusActie(Richting.RECHTS);
-            } else {
-                speler.beweegwActie(Richting.RECHTS);
-            }
-            view.imageSetter(veld,speler);
-        });
-
-        btnDOWN.setOnAction(event -> {
-            if(hakken) {
-                speler.muurInteractie(Richting.ONDER);
-            }else if(deurinteractie){
-                speler.deurInteractie(Richting.ONDER);
-            }else if(brandblusser){
-                speler.blusActie(Richting.ONDER);
-            } else {
-                speler.beweegwActie(Richting.ONDER);
-            }
-            view.imageSetter(veld,speler);
-        });
-
-        imgHakken.setOnMouseClicked(event -> {
-            if(hakken) {
-                hakken = false;
-            } else {
-                hakken = true;
-                deurinteractie = false;
-                brandblusser = false;
-            }
-            System.out.println(hakken);
-        });
-
-        imgOpenendeur.setOnMouseClicked(event -> {
-            if(deurinteractie) {
-                deurinteractie = false;
-            } else {
-                hakken = false;
-                deurinteractie = true;
-                brandblusser = false;
-            }
-            System.out.println(deurinteractie);
-        });
-
-        imgBrandblusser.setOnMouseClicked(event -> {
-            if(brandblusser) {
-                brandblusser = false;
-            } else {
-                hakken = false;
-                deurinteractie = false;
-                brandblusser = true;
-            }
-            System.out.println(deurinteractie);
-        });
-
-        btnSpecial.setOnAction(event -> {
-//            for (int vaak = 0; vaak < 3; vaak++) {
-//
-//                ba.nieuwRook();
-//                ba.hanteerVonkoverslag();
-//                ba.hanteerStoffen();
-//                view.imageSetter(veld,speler);
-//            }
-            ba.nieuwRook();
-            view.imageSetter(veld,speler);
-        });
-
-
-
     }
+
+
+
+    EventHandler listenForCalcButton = event -> System.out.println("Works?");
+
 }
--- Flashspace/src/sample/View.java
+++ Flashspace/src/sample/View.java
@@ -1,47 +1,93 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.GridPane;
 
-public class View{
+import java.awt.event.ActionListener;
+
+public class View {
+    @FXML
+    private GridPane gridpane;
+    @FXML
+    private Button btnUP;
+    @FXML
+    private Button btnLEFT;
+    @FXML
+    private Button btnRIGHT;
+    @FXML
+    private Button btnDOWN;
+    @FXML
+    private ImageView imgHakken;
+    @FXML
+    private ImageView imgOpenendeur;
+    @FXML
+    private ImageView imgBrandblusser;
+    @FXML
+    private Button btnSpecial;
+
     FlowPane[]flowPanes = new FlowPane[80];
     ImageView[]imageViews = new ImageView[720];
+    Speelveld veld = new Speelveld();
+    Speler speler2 = new Speler("Norddin",Kleur.ORANJE,3,4,veld);
 
-    Image empty = new Image("sample/gfx/Empty.png",20,20,false,true);
-    Image hotspot = new Image("sample/gfx/Hotspot.png",20,20,false,true);
-    Image persoon = new Image("sample/gfx/Vraagteken.png",20,20,false,true);
-    Image vlam = new Image("sample/gfx/Vlam.png",20,20,false,true);
-    Image rook = new Image("sample/gfx/Rook.png",20,20,false,true);
-    Image gevaarlijkeStof = new Image("sample/gfx/GevaarlijkeStof.png",20,20,false,true);
-    Image dichteDeurLinks = new Image("sample/gfx/DichteDeurLinks.png",20,20,false,true);
-    Image dichteDeurOnder = new Image("sample/gfx/DichteDeurOnder.png",20,20,false,true);
-    Image dichteDeurBoven = new Image("sample/gfx/DichteDeurBoven.png",20,20,false,true);
-    Image dichteDeurRechts = new Image("sample/gfx/DichteDeurRechts.png",20,20,false,true);
-    Image openDeurLinks = new Image("sample/gfx/OpenDeurLinks.png",20,20,false,true);
-    Image openDeurOnder = new Image("sample/gfx/OpenDeurOnder.png",20,20,false,true);
-    Image openDeurBoven = new Image("sample/gfx/OpenDeurBoven.png",20,20,false,true);
-    Image openDeurRechts = new Image("sample/gfx/OpenDeurRechts.png",20,20,false,true);
-    Image muurBoven = new Image("sample/gfx/MuurBoven.png",20,20,false,true);
-    Image muurLinks = new Image("sample/gfx/MuurLinks.png",20,20,false,true);
-    Image muurOnder = new Image("sample/gfx/MuurOnder.png",20,20,false,true);
-    Image muurRechts = new Image("sample/gfx/MuurRechts.png",20,20,false,true);
-    Image muur1kapotBoven = new Image("sample/gfx/Muur1kapotBoven.png",20,20,false,true);
-    Image muur1kapotLinks = new Image("sample/gfx/Muur1kapotLinks.png",20,20,false,true);
-    Image muur1kapotOnder = new Image("sample/gfx/Muur1kapotOnder.png",20,20,false,true);
-    Image muur1kapotRechts = new Image("sample/gfx/Muur1kapotRechts.png",20,20,false,true);
-    Image muur2kapotBoven = new Image("sample/gfx/Muur2kapotBoven.png",20,20,false,true);
-    Image muur2kapotLinks = new Image("sample/gfx/Muur2kapotLinks.png",20,20,false,true);
-    Image muur2kapotOnder = new Image("sample/gfx/Muur2kapotOnder.png",20,20,false,true);
-    Image muur2kapotRechts = new Image("sample/gfx/Muur2kapotRechts.png",20,20,false,true);
-    Image brandweerBlauw = new Image("sample/gfx/Brandweerblauw.png",20,20,false,true);
-    Image brandweerGeel = new Image("sample/gfx/Brandweergeel.png",20,20,false,true);
-    Image brandweerGroen = new Image("sample/gfx/Brandweergroen.png",20,20,false,true);
-    Image brandweerOranje = new Image("sample/gfx/Brandweeroranje.png",20,20,false,true);
-    Image brandweerRood = new Image("sample/gfx/Brandweerrood.png",20,20,false,true);
-    Image brandweerZwart = new Image("sample/gfx/Brandweerzwart.png",20,20,false,true);
+    public View() {
+        imageViewsFactory();
+        flowPanesFactory();
+    }
+
+    public void initialize() {
+        imageSetter(veld,speler2);
+
+        btnUP.setOnAction(event -> {
+
+        });
+
+    }
 
 
+
+    public void addCalculationListener(EventHandler listenForCalcButton) {
+        btnUP.setOnAction(listenForCalcButton);
+    }
+
+    Image empty = new Image("sample/gfx/Empty.png", 20, 20, false, true);
+    Image hotspot = new Image("sample/gfx/Hotspot.png", 20, 20, false, true);
+    Image persoon = new Image("sample/gfx/Vraagteken.png", 20, 20, false, true);
+    Image vlam = new Image("sample/gfx/Vlam.png", 20, 20, false, true);
+    Image rook = new Image("sample/gfx/Rook.png", 20, 20, false, true);
+    Image gevaarlijkeStof = new Image("sample/gfx/GevaarlijkeStof.png", 20, 20, false, true);
+    Image dichteDeurLinks = new Image("sample/gfx/DichteDeurLinks.png", 20, 20, false, true);
+    Image dichteDeurOnder = new Image("sample/gfx/DichteDeurOnder.png", 20, 20, false, true);
+    Image dichteDeurBoven = new Image("sample/gfx/DichteDeurBoven.png", 20, 20, false, true);
+    Image dichteDeurRechts = new Image("sample/gfx/DichteDeurRechts.png", 20, 20, false, true);
+    Image openDeurLinks = new Image("sample/gfx/OpenDeurLinks.png", 20, 20, false, true);
+    Image openDeurOnder = new Image("sample/gfx/OpenDeurOnder.png", 20, 20, false, true);
+    Image openDeurBoven = new Image("sample/gfx/OpenDeurBoven.png", 20, 20, false, true);
+    Image openDeurRechts = new Image("sample/gfx/OpenDeurRechts.png", 20, 20, false, true);
+    Image muurBoven = new Image("sample/gfx/MuurBoven.png", 20, 20, false, true);
+    Image muurLinks = new Image("sample/gfx/MuurLinks.png", 20, 20, false, true);
+    Image muurOnder = new Image("sample/gfx/MuurOnder.png", 20, 20, false, true);
+    Image muurRechts = new Image("sample/gfx/MuurRechts.png", 20, 20, false, true);
+    Image muur1kapotBoven = new Image("sample/gfx/Muur1kapotBoven.png", 20, 20, false, true);
+    Image muur1kapotLinks = new Image("sample/gfx/Muur1kapotLinks.png", 20, 20, false, true);
+    Image muur1kapotOnder = new Image("sample/gfx/Muur1kapotOnder.png", 20, 20, false, true);
+    Image muur1kapotRechts = new Image("sample/gfx/Muur1kapotRechts.png", 20, 20, false, true);
+    Image muur2kapotBoven = new Image("sample/gfx/Muur2kapotBoven.png", 20, 20, false, true);
+    Image muur2kapotLinks = new Image("sample/gfx/Muur2kapotLinks.png", 20, 20, false, true);
+    Image muur2kapotOnder = new Image("sample/gfx/Muur2kapotOnder.png", 20, 20, false, true);
+    Image muur2kapotRechts = new Image("sample/gfx/Muur2kapotRechts.png", 20, 20, false, true);
+    Image brandweerBlauw = new Image("sample/gfx/Brandweerblauw.png", 20, 20, false, true);
+    Image brandweerGeel = new Image("sample/gfx/Brandweergeel.png", 20, 20, false, true);
+    Image brandweerGroen = new Image("sample/gfx/Brandweergroen.png", 20, 20, false, true);
+    Image brandweerOranje = new Image("sample/gfx/Brandweeroranje.png", 20, 20, false, true);
+    Image brandweerRood = new Image("sample/gfx/Brandweerrood.png", 20, 20, false, true);
+    Image brandweerZwart = new Image("sample/gfx/Brandweerzwart.png", 20, 20, false, true);
 
     void imageViewsFactory() {
         for(int i = 0; i<720; i++) {
@@ -144,27 +190,6 @@ public class View{
                 } else {
                     imageViews[q].setImage(empty);
                 }
-//                for(int i = 0; i<6; i++) {
-//                    if(veld.getVak(x,y).getKleuren(i) != null) {
-//                        switch(veld.getVak(x,y).getKleuren(i)) {
-//                            case GEEL: imageViews[q].setImage(brandweerGeel);
-//                                break;
-//                            case ROOD: imageViews[q].setImage(brandweerRood);
-//                                break;
-//                            case BLAUW: imageViews[q].setImage(brandweerBlauw);
-//                                break;
-//                            case GROEN: imageViews[q].setImage(brandweerGroen);
-//                                break;
-//                            case ZWART: imageViews[q].setImage(brandweerZwart);
-//                                break;
-//                            case ORANJE: imageViews[q].setImage(brandweerOranje);
-//                                break;
-//                            default:
-//                                System.out.println("How did this even happen :|");
-//                                break;
-//                        }
-//                    }
-//                }
                 q++;
 
                 //Obstakel rechts
@@ -222,186 +247,4 @@ public class View{
             }
         }
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//    ImageView[]ivarray = new ImageView[720];
-//    FlowPane[][]fp;
-//    Speelveld veld;
-//    Image muurOnder;
-//    Image muurLinks;
-//    Image muurBoven;
-//    Image muurRechts;
-//
-//
-//    public View(FlowPane[][] fp, Speelveld veld) {
-//        this.fp = fp;
-//        this.veld = veld;
-//        for (int i = 0; i<720; i++) {
-//            ivarray[i] = new ImageView(new Image("sample/gfx/Empty.png", 20, 20, false, true));
-//        }
-//    }
-//
-//    public void update() {
-//        makeObstakels();
-//        int count = 0;
-//        for(int x = 0; x <10; x++) {
-//            for (int y = 0; y < 8; y++) {
-//                for (int z = 0; z < 9; z++) {
-//                    if (z == 0) {
-//                        fp[x][y].getChildren().clear();
-//                    }
-//                    if (veld.getVak(x,y).getObjecten()[z] != null) {
-//                        fp[x][y].getChildren().add(veld.getVak(x, y).getObjecten()[z].getImageView());
-//                    }
-//                    else {
-//                        fp[x][y].getChildren().add(ivarray[count]);
-//                        count++;
-//                    }
-//
-//                }
-//            }
-//        }
-//    }
-//
-//    public void makeObstakels() {
-//        for(int x = 0; x <10; x++) {
-//            for (int y = 0; y < 8; y++) {
-//                for (int z = 0; z < 4; z++) {
-//                    switch(veld.getVak(x,y).checkObstakels(z)) {
-//                        case 0:
-//                            switch(z) {
-//                                case 0:
-//                                    makeWallsAssistent(x, y, z, "sample/gfx/MuurOnder.png");
-//                                    break;
-//                                case 1:
-//                                    makeWallsAssistent(x, y, z, "sample/gfx/MuurLinks.png");
-//                                    break;
-//                                case 2:
-//                                    makeWallsAssistent(x, y, z, "sample/gfx/MuurBoven.png");
-//                                    break;
-//                                case 3:
-//                                    makeWallsAssistent(x, y, z, "sample/gfx/MuurRechts.png");
-//                                    break;
-//                            }
-//                            break;
-//                        case 1:
-//                            switch(z) {
-//                                case 0:
-//                                    makeWallsAssistent(x, y, z, "sample/gfx/Muur1kapotOnder.png");
-//                                    break;
-//                                case 1:
-//                                    makeWallsAssistent(x, y, z, "sample/gfx/Muur1kapotLinks.png");
-//                                    break;
-//                                case 2:
-//                                    makeWallsAssistent(x, y, z, "sample/gfx/Muur1kapotBoven.png");
-//                                    break;
-//                                case 3:
-//                                    makeWallsAssistent(x, y, z, "sample/gfx/Muur1kapotRechts.png");
-//                                    break;
-//                            }
-//                            break;
-//                        case 2:
-//                            switch(z) {
-//                                case 0:
-//                                    makeWallsAssistent(x, y, z, "sample/gfx/Muur2kapotOnder.png");
-//                                    break;
-//                                case 1:
-//                                    makeWallsAssistent(x, y, z, "sample/gfx/Muur2kapotLinks.png");
-//                                    break;
-//                                case 2:
-//                                    makeWallsAssistent(x, y, z, "sample/gfx/Muur2kapotBoven.png");
-//                                    break;
-//                                case 3:
-//                                    makeWallsAssistent(x, y, z, "sample/gfx/Muur2kapotRechts.png");
-//                                    break;
-//                            }
-//                            break;
-//                        case 3:
-//                            switch(z) {
-//                                case 0:
-//                                    makeDoorsAssistent(x, y, z, "sample/gfx/DichteDeurOnder.png");
-//                                    break;
-//                                case 1:
-//                                    makeDoorsAssistent(x, y, z, "sample/gfx/DichteDeurLinks.png");
-//                                    break;
-//                                case 2:
-//                                    makeDoorsAssistent(x, y, z, "sample/gfx/DichteDeurBoven.png");
-//                                    break;
-//                                case 3:
-//                                    makeDoorsAssistent(x, y, z, "sample/gfx/DichteDeurRechts.png");
-//                                    break;
-//                            }
-//                            break;
-//                        case 4:
-//                            switch(z) {
-//                                case 0:
-//                                    makeDoorsAssistent(x, y, z, "sample/gfx/OpenDeurOnder.png");
-//                                    break;
-//                                case 1:
-//                                    makeDoorsAssistent(x, y, z, "sample/gfx/OpenDeurLinks.png");
-//                                    break;
-//                                case 2:
-//                                    makeDoorsAssistent(x, y, z, "sample/gfx/OpenDeurBoven.png");
-//                                    break;
-//                                case 3:
-//                                    makeDoorsAssistent(x, y, z, "sample/gfx/OpenDeurRechts.png");
-//                                    break;
-//                            }
-//                            break;
-//                    }
-//                }
-//            }
-//        }
-//    }
-//
-//    private void makeWallsAssistent(int x, int y, int z, String png) {
-//        if (z ==0) {
-//            veld.getVak(x,y).removeObject(1);
-//            veld.getVak(x,y).addMuur(new Object.Muur(new ImageView(new Image(png,20,20,false,true))),z);
-//        }
-//        if (z ==1) {
-//            veld.getVak(x,y).removeObject(5);
-//            veld.getVak(x,y).addMuur(new Object.Muur(new ImageView(new Image(png,20,20,false,true))),z);
-//        }
-//        if (z ==2) {
-//            veld.getVak(x,y).removeObject(7);
-//            veld.getVak(x,y).addMuur(new Object.Muur(new ImageView(new Image(png,20,20,false,true))),z);
-//        }
-//        if (z ==3) {
-//            veld.getVak(x,y).removeObject(3);
-//            veld.getVak(x,y).addMuur(new Object.Muur(new ImageView(new Image(png,20,20,false,true))),z);
-//        }
-//    }
-//
-//    private void makeDoorsAssistent(int x, int y, int z, String png) {
-//        if (z ==0) {
-//            veld.getVak(x,y).addMuur(new Object.Deur(new ImageView(new Image(png,20,20,false,true))),z);
-//        }
-//        if (z ==1) {
-//            veld.getVak(x,y).addMuur(new Object.Deur(new ImageView(new Image(png,20,20,false,true))),z);
-//        }
-//        if (z ==2) {
-//            veld.getVak(x,y).addMuur(new Object.Deur(new ImageView(new Image(png,20,20,false,true))),z);
-//        }
-//        if (z ==3) {
-//            veld.getVak(x,y).addMuur(new Object.Deur(new ImageView(new Image(png,20,20,false,true))),z);
-//        }
-//    }
 }
