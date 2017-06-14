6aa3a9310a8f22e5890aebcb2f362da17849cc07
Joepin8 <joep_oonk@hotmail.com>
2017-06-14 12:50:08 +0200

    New Project

--- Operation Flashpoint/src/View/LobbyView.java
+++ Operation Flashpoint/src/View/LobbyView.java
@@ -3,7 +3,10 @@ package View;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
@@ -13,11 +16,18 @@ public class LobbyView extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Pane pane = new Pane();
-        Scene scene = new Scene(pane);
+
+        Button oke = new Button("OKE");
+        VBox vbox = new VBox();
+        vbox.getChildren().addAll(oke);
 
 
+        BorderPane borderPane = new BorderPane();
 
+        borderPane.setCenter(vbox);
+
+        Pane pane = new Pane(borderPane);
+        Scene scene = new Scene(pane, 300, 300);
         primaryStage.setScene(scene);
         primaryStage.setTitle("Lobby");
         primaryStage.show();
