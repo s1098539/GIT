89d71c940f2e3730dcba01b622fb1c5c133e18d5
Lion <s1098539@student.hsleiden.nl>
2017-06-15 14:22:16 +0200

    SpeelveldController checkVakSpeler

--- Operation Flashpoint/src/Controller/SpeelveldController.java
+++ Operation Flashpoint/src/Controller/SpeelveldController.java
@@ -5,6 +5,9 @@ import Model.Fiche;
 import Model.Richting;
 import Model.Speelveld;
 import Model.Status;
+import Model.Kleur;
+
+import java.util.ArrayList;
 
 public class SpeelveldController {
     Speelveld speelveld = new Speelveld();
@@ -43,6 +46,15 @@ public class SpeelveldController {
 
     }
 
-
+    // Lion, kijkt of een speler zich op een bepaald vak bevind en geeft al die speler kleuren terug in een arraylist
+    private ArrayList<Kleur> checkVakSpeler(int x, int y) {
+        ArrayList<Kleur> kleuren= new ArrayList<>();
+        for (int i = 0; i < 6; i++) {
+            if(speelveld.getVakken()[x][y].getKleuren()[i] != null) {
+                kleuren.add(speelveld.getVakken()[x][y].getKleuren()[i]);
+            }
+        }
+        return kleuren;
+    }
 
 }
