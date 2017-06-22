5e0d1942f8116be42f695f0fd875fa0080055895
DOGGO <johndoe@example.com>
2017-06-22 18:24:10 +0200

    YOU CAN PULL THIS TIME CALVIN I SWEAR

--- Operation Flashpoint/src/Main/Interface.java
+++ Operation Flashpoint/src/Main/Interface.java
@@ -1,16 +1,15 @@
 package Main;
 
+import Controller.SpelController;
 import Model.*;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface Interface extends Remote {
-    //-- Speler (Naam, Kleur, x, y, ap, ep, rol, slechtziendmodus, stof, persoon)
-    public Parse sendParse() throws RemoteException;
-    //--
+    public Parse getParse() throws RemoteException;
 
-    //-- Speelveld
+    public String getMessage() throws RemoteException;
 
-    //--
+    public void setMessage(String string) throws RemoteException;
 }
--- Operation Flashpoint/src/Main/Main2.java
+++ Operation Flashpoint/src/Main/Main2.java
@@ -93,8 +93,6 @@ public class Main2 extends Application{
 
             //TODO DEBUGLINES
             System.out.println("DEBUGGING STARTED\n \n \nRESPONSES BELOW THIS LINE. \n............................." );
-            clientStub.sendParse().setVeldD(veldC.getVeldD());
-            System.out.println(clientStub.sendParse().getVeldD());
         } catch (Exception e) {
             System.out.println("EXCEPTION: " + e);
         }
